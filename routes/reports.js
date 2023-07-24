427806b5b91e61356d136726ff3327a842132a24
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-07-24 19:55:34 -0400

    parsing header

--- routes/reports.js
+++ routes/reports.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 
 // Create a new report
 router.post('/', async (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     try {
       const { author_names,
       affiliations,
